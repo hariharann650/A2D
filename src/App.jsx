d8e3c7aaa05a7cf7bb9365fcc9a7bd619d1a1009
HARIHARAN P <114478496+hariharann650@users.noreply.github.com>
2024-11-09 13:24:04 +0530

    alter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import Index from ".";
+// import Index from ".";
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Form from './Form/Form';
+
+// function App() {
+
+//   return (
+//     <Router>
+//     <Routes>
+//       <Route path="/" element={<Index /> } />
+//       <Route path="/Form" element={<Form/>} />
+//     </Routes>
+//   </Router>
+  
+   
+//   )
+// }
+
+// export default App;
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Index from './index'; // Update path to the exact location of Index component
 import Form from './Form/Form';
 
 function App() {
-
   return (
-    <Router>
-    <Routes>
-      <Route path="/" element={<Index /> } />
-      <Route path="/Form" element={<Form/>} />
-    </Routes>
-  </Router>
-  
-   
-  )
+    <Router basename="A2D">  {/* Replace 'your-repo-name' with your actual GitHub repository name */}
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/form" element={<Form />} /> {/* Standardize route naming */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
 
 
 
